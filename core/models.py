a443f5bf7243734863da1bec3fb1bc3ca10f86b6
tfiers <tomas.fiers@gmail.com>
2015-01-06 14:34:22 +0100

    (Cosmetic: removed unused commented-out import.)

--- core/models.py
+++ core/models.py
@@ -3,7 +3,6 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from django.utils import timezone
 
-# from django.contrib import auth
 from django.contrib.auth.models import (
     AbstractBaseUser, PermissionsMixin, BaseUserManager
 )
