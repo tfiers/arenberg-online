9c9317042ba0c1cbfcc484fb67974074b51c4ed0
tfiers <tomas.fiers@gmail.com>
2015-01-06 09:51:06 +0100

    Hid Secret Key in environment variable.
    'ARENBERG_ONLINE_DJANGO_SECRET_KEY'.

--- arenberg_online/settings.py
+++ arenberg_online/settings.py
@@ -16,8 +16,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'r*&ua!e)y4x*vtb@t**s&ujny2n(&!^whbep7*q4ivub^ghjo@'
+SECRET_KEY = os.environ['ARENBERG_ONLINE_DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
