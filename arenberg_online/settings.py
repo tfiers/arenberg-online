dcd693e1744eb78ecf088be56baab351f4135f20
root <tomas.fiers@gmail.com>
2015-04-17 08:17:56 +0200

    Set settings flag to production.

--- arenberg_online/settings.py
+++ arenberg_online/settings.py
@@ -8,7 +8,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.7/ref/settings/
 """
 
-DEVELOPPING = True
+DEVELOPPING = False
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
