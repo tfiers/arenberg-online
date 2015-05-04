5e74391902af1ee719dd66387ddcaabbf1e10eea
tfiers <tomas.fiers@gmail.com>
2015-05-04 19:04:22 +0200

    More items per page in ticketing admin

--- ticketing/admin.py
+++ ticketing/admin.py
@@ -7,9 +7,11 @@ class OrderAdmin(admin.ModelAdmin):
 	list_display = ('first_name', 'last_name', 'seller', 'num_tickets', 'total_price',
 		'performance', 'payment_method', 'email', 'date', 'user_remarks', 'admin_remarks')
 	ordering = ('-date',)
+	list_per_page = 1000
 
 class StandardMarketingPollAnswerAdmin(admin.ModelAdmin):
 	list_display = ('associated_order', 'marketing_feedback', 'referred_member', 'first_concert')
+	list_per_page = 1000
 
 admin.site.register(Production)
 admin.site.register(Performance)
