5adfa74c64ad7859438df97c0065b8ff4435890e
lennartbu <lennart.bulteel@student.kuleuven.be>
2015-12-21 18:10:50 +0100

    registration now has working email confirmation, so it's FINISHED!

--- core/forms.py
+++ core/forms.py
@@ -15,20 +15,36 @@ class UserForm(forms.ModelForm):
 
     error_messages = {
         'password_mismatch': _("The two password fields didn't match."),
+        'email_mismatch': _("The two e-mail fields didn't match."),
     }
     password1 = forms.CharField(label=_("Password"),
         widget=forms.PasswordInput)
     password2 = forms.CharField(label=_("Password confirmation"),
         widget=forms.PasswordInput,
         help_text=_("Enter the same password as before, for verification."))
+    email = forms.EmailField(label=_("e-mail"))
+    email2 = forms.EmailField(label=_("e-mail opnieuw"))
+
 
     class Meta:
         model = User
-        fields = ("email","first_name","last_name")
+        fields = ("first_name","last_name")
 
     def __init__(self, *args, **kwargs):
         super(UserForm, self).__init__(*args, **kwargs)
-        self.fields['email'].widget.attrs.update({'autofocus': ''})
+        #self.fields['email'].widget.attrs.update({'autofocus': ''})
+
+    def clean_email2(self):
+        email1 = self.cleaned_data.get("email")
+        email2 = self.cleaned_data.get("email2")
+        if email1 and email2 and email1 != email2:
+            raise forms.ValidationError(
+                self.error_messages['email_mismatch'],
+                code='email_mismatch',
+            )
+        #self.instance.email = self.cleaned_data.get('email')
+        #password_validation.validate_password(self.cleaned_data.get('password2'), self.instance) #password validation, like check whether >6 chars and such, cannot import it from auth, outdated django?
+        return email2
 
     def clean_password2(self):
         password1 = self.cleaned_data.get("password1")
@@ -38,13 +54,14 @@ class UserForm(forms.ModelForm):
                 self.error_messages['password_mismatch'],
                 code='password_mismatch',
             )
-        self.instance.email = self.cleaned_data.get('email')
-        #password_validation.validate_password(self.cleaned_data.get('password2'), self.instance)
+        #self.instance.email = self.cleaned_data.get('email')
+        #password_validation.validate_password(self.cleaned_data.get('password2'), self.instance) #password validation, like check whether >6 chars and such, cannot import it from auth, outdated django?
         return password2
 
     def save(self, commit=True):
         user = super(UserForm, self).save(commit=False)
         user.set_password(self.cleaned_data["password1"])
+        user.email = self.cleaned_data['email']
         if commit:
             user.save()
         return user
